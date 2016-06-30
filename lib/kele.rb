19afc3d0470529ad3128e7a52e854d75b15083bc
pnb3431 <pete.bailen@gmail.com>
2016-06-30 03:39:35 +0000

    user story 4 complete

--- lib/kele.rb
+++ lib/kele.rb
@@ -1,10 +1,12 @@
 require "httparty"
 require "kele/errors"
 require "json"
+require "kele/roadmap"
 
 
 class Kele
     include HTTParty
+    include Roadmap
     def initialize(email, password)
         response = self.class.post(base_api_endpoint("sessions"), body: { "email": email, "password": password })
         @auth_token = response["auth_token"]
@@ -21,6 +23,8 @@ class Kele
         response = self.class.get(base_api_endpoint("mentors/#{mentor_id}/student_availability"), headers: { "authorization" => @auth_token })
         @mentor_availability = JSON.parse(response.body)
     end
+    
+    
 
 
     private
