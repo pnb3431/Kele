ba9b18dfd0ab53c36fd4c9820fe86221c1cff83b
pnb3431 <pete.bailen@gmail.com>
2016-07-05 15:40:27 +0000

    finished user story 5

--- lib/kele.rb
+++ lib/kele.rb
@@ -4,6 +4,7 @@ require "json"
 require "kele/roadmap"
 
 
+
 class Kele
     include HTTParty
     include Roadmap
@@ -22,6 +23,27 @@ class Kele
     def get_mentor_availability(mentor_id)
         response = self.class.get(base_api_endpoint("mentors/#{mentor_id}/student_availability"), headers: { "authorization" => @auth_token })
         @mentor_availability = JSON.parse(response.body)
+    end
+    
+    def get_messages(*page)
+        if page.empty?
+            response = self.class.get(base_api_endpoint("message_threads"), headers: { "authorization" => @auth_token })
+            @messages = (1..(response["count"]/10 + 1)).map do |i|
+                self.class.get(base_api_endpoint("message_threads?page=#{i}"), headers: { "authorization" => @auth_token })
+                
+            end
+               puts @messages
+            
+        else
+            response = self.class.get(base_api_endpoint("message_threads?page=#{page.join.to_i}"), headers: { "authorization" => @auth_token })
+            @messages = JSON.parse(response.body)
+            
+        end
+    end
+    
+    def create_message(user_id, recipient_id, token, subject, stripped)
+        message_data = {body: {user_id: user_id, recipient_id: recipient_id, token: nil, subject: subject, stripped: stripped}, headers: { "authorization" => @auth_token }}
+        self.class.post(base_api_endpoint("messages"), message_data)
     end
     
     
