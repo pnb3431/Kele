a432da5ac7f4e3ae015649e5fc064f14441d09ae
pnb3431 <pete.bailen@gmail.com>
2016-07-05 15:59:07 +0000

    user story 6 complete

--- lib/kele.rb
+++ lib/kele.rb
@@ -46,6 +46,11 @@ class Kele
         self.class.post(base_api_endpoint("messages"), message_data)
     end
     
+    def create_submission(checkpoint_id, assignment_branch, assignment_commit_link, comment, enrollment_id)
+        submisson_data = {body: {checkpoint_id: checkpoint_id, assignment_branch: assignment_branch, assignment_commit_link: assignment_commit_link, comment: comment, enrollment_id: enrollment_id}, headers: { "authorization" => @auth_token }}
+        self.class.post(base_api_endpoint("checkpoint_submissions"), submisson_data)
+    end
+    
     
 
 
